d3ff02a461576f1d11d7879200e6ba25a43a9c47
cmhmtgmods <devincrmp01@gmail.com>
2025-04-18 14:52:44 +0300

    cfg

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withNextIntl = require('next-intl/plugin')();
+
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['via.placeholder.com'],
-      formats: ['image/avif', 'image/webp'],
-    },
-    i18n: {
-      locales: ['en', 'ru', 'bg'],
-      defaultLocale: 'ru',
-      localeDetection: true,
-    },
-    experimental: {
-      scrollRestoration: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  images: {
+    domains: ['via.placeholder.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
